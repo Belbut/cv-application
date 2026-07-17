d5d54e42f71a9015f8c02b68b4bf723fd537da7f
Belbut <80793911+Belbut@users.noreply.github.com>
2026-07-17 22:14:16 +0100

    refactor: education component multiple entries

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,19 +1,50 @@
 import Info from "./Info.jsx"
 import { useState } from "react"
 
+function Element({ content, editContent }) {
+
+  return (
+    <div className="course">
+      <Info info={content} setInfo={editContent} contentType="school" />
+      <Info info={content} setInfo={editContent} contentType="course" />
+      <Info info={content} setInfo={editContent} contentType="startDate" />
+      <Info info={content} setInfo={editContent} contentType="endDate" />
+    </div>
+  )
+}
+
 function EducationInfo() {
-  const [info, setInfo] = useState({ school: "Hogwards", course: "magic", startDate: "17/07/2020", endDate: "17/07/2026" })
+  const [info, setInfo] = useState([{ id: 0, school: "Hogwards", course: "magic", startDate: "17/07/2020", endDate: "17/07/2026" }])
+
+  const findRecord = (id) => {
+    return info.find((school) => school.id == id)
+  }
+
+  const editRecord = (id) => {
+    return (fn) => {
+      setInfo(
+        info.map(school => {
+          return school.id == id ? fn(school) : school
+        })
+      )
+    }
+  }
+
+  const renderRecordList = () => {
+    return info.map((r) => {
+      return (<Element
+        key={r.id}
+        content={findRecord(r.id)}
+        editContent={editRecord(r.id)}
+      />)
+    })
+  }
 
   return (
     <>
       <h2>Education</h2>
       <div className="education-information">
-        <div className="course">
-          <Info info={info} setInfo={setInfo} contentType="school" />
-          <Info info={info} setInfo={setInfo} contentType="course" />
-          <Info info={info} setInfo={setInfo} contentType="startDate" />
-          <Info info={info} setInfo={setInfo} contentType="endDate" />
-        </div>
+        {renderRecordList()}
       </div>
     </>
   )
